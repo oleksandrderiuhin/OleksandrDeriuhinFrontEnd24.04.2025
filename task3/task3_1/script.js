a60d7f55b594b75b792bbc525e3d420c98298916
Oleksandr Deriuhingit config --global email.name oleksandrderiuhin97@gmail.com <oleksandrderiuhin97@gmail.com>
2025-05-03 16:42:04 +0300

    Add first homework_3.1

---
+++ task3/task3_1/script.js
@@ -0,0 +1,20 @@
+const num= 1;
+const str ="I love Front-End";
+const bigNumber = 12345n;
+const isLogged = true;
+const emptySpace = null;
+let variable;
+const symbol=Symbol();
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bigNumber));
+console.log(typeof(isLogged));
+console.log(typeof(emptySpace));
+console.log(typeof(variable));
+console.log(typeof(symbol));
+console.log(typeof(function (){}));
+
+
+
+
