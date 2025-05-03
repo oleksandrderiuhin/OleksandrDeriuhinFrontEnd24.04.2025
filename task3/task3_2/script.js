aa0c623952eab85e9e1d54885a85dfca48d07748
Oleksandr Deriuhingit config --global email.name oleksandrderiuhin97@gmail.com <oleksandrderiuhin97@gmail.com>
2025-05-03 17:07:35 +0300

    Added homework_3.2

---
+++ task3/task3_2/script.js
@@ -0,0 +1,8 @@
+const name=prompt('Enter your name:', 'Oleksandr');
+const surname=prompt('Enter your surname:', 'Deriuhin');
+const course=prompt('Enter your course:', 'Front-End Pro');
+
+
+console.log(name);
+console.log(surname);
+console.log(course);
