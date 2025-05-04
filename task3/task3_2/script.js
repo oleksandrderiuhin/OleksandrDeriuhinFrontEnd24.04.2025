d829cfc5f15c3e95a4d9a28afc795d990bb64b58
Oleksandr Deriuhingit config --global email.name oleksandrderiuhin97@gmail.com <oleksandrderiuhin97@gmail.com>
2025-05-04 23:34:14 +0300

    Add space between prompt

--- task3/task3_2/script.js
+++ task3/task3_2/script.js
@@ -1,6 +1,6 @@
-const name=prompt('Enter your name:', 'Oleksandr');
-const surname=prompt('Enter your surname:', 'Deriuhin');
-const course=prompt('Enter your course:', 'Front-End Pro');
+const name= prompt('Enter your name:', 'Oleksandr');
+const surname= prompt('Enter your surname:', 'Deriuhin');
+const course= prompt('Enter your course:', 'Front-End Pro');
 
 
 console.log(name);
