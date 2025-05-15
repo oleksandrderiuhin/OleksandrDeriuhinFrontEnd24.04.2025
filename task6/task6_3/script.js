174cdd2e089d630ac57d108e307daf9c7cb8b589
Oleksandr Deriuhingit config --global email.name oleksandrderiuhin97@gmail.com <oleksandrderiuhin97@gmail.com>
2025-05-16 01:17:14 +0300

    Added task_6.3

--- task6/task6_3/script.js
+++ task6/task6_3/script.js
@@ -1,17 +1,34 @@
-const array = [{}, 1, 2, 3, 4, 5, 6, "string", null];
+// function removeElement(array, item) {
+//     let writeIndex = 0;
+//
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] !== item) {
+//             array[writeIndex] = array[i];
+//             writeIndex++;
+//         }
+//     }
+//
+//     array.length = writeIndex;
+// }
+//
+// const array = [1, "text", { key: "value" }, true, "text", null, 42];
+// removeElement(array, array[3]);
+// removeElement(array, true);
+// removeElement(array, 42);
+//
+// console.log(array);
 
-function foo(array) {
-    const numbers = array.filter(Number.isFinite);
-    if (!numbers.length) return 0;
+/////////////////////////////////////////////////////////////////////////////
 
-    let sum = 0;
-    for (let num of numbers) {
-        sum += num;
+function removeElement(array, item) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+        }
     }
-
-    return sum / numbers.length;
+    return array;
 }
 
-const result = foo(array);
 
-console.log(result); // Виведе: 3.5
+const array = [1, 2, 3, 2, 4, 2, 5];
+console.log(removeElement(array, 1));
