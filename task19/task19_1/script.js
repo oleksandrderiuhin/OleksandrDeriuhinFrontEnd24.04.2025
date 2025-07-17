b4d08421aca259dcdd62dcf96e00f0a9eebb3a70
Oleksandr Deriuhingit config --global email.name oleksandrderiuhin97@gmail.com <oleksandrderiuhin97@gmail.com>
2025-07-17 16:26:24 +0300

    Added task 19_1

---
+++ task19/task19_1/script.js
@@ -0,0 +1,26 @@
+const apiKey = "3bb87e89fc1863555c6b201a76af663b";
+const city = "Kyiv";
+
+async function getWeather() {
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ua`
+        );
+
+        //if (!response.ok) throw new Error("Помилка завантаження даних");
+
+        const data = await response.json();
+
+       // console.log(data);
+
+        document.getElementById("temperature").textContent = `Температура: ${data.main.temp}°C`;
+        document.getElementById("description").textContent = `Опис: ${data.weather[0].description}`;
+    } catch (error) {
+        document.getElementById("temperature").textContent = "Помилка завантаження погоди";
+        document.getElementById("description").textContent = "";
+        console.error(error);
+    }
+}
+
+// завантажити погоду одразу при запуску
+getWeather();
