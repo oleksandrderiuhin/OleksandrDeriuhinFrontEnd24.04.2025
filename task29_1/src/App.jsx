4f6081a6941ab951d790ccb44123299094fe7650
Oleksandr Deriuhingit config --global email.name oleksandrderiuhin97@gmail.com <oleksandrderiuhin97@gmail.com>
2025-08-28 15:14:24 +0300

    Added task29_1

---
+++ task29_1/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+const App = () => {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Counter: {count}</h1>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(increment())}>+</button>
+        </div>
+    );
+};
+
+export default App;
+
