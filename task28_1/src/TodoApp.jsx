85497525d8b41e5a1e01d3c2646cb0ba9a7f7e4b
Oleksandr Deriuhingit config --global email.name oleksandrderiuhin97@gmail.com <oleksandrderiuhin97@gmail.com>
2025-08-28 14:42:16 +0300

    Added task28_1

---
+++ task28_1/src/TodoApp.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const TodoApp = () => {
+    const [todos, setTodos] = useState([]);
+
+    // Валідація
+    const validationSchema = Yup.object({
+        task: Yup.string()
+            .min(5, "Мінімум 5 символів")
+            .required("Поле обов'язкове"),
+    });
+
+    return (
+        <div className="app">
+            <h1>TODO List з Formik</h1>
+
+            {/* Форма */}
+            <Formik
+                initialValues={{ task: "" }}
+                validationSchema={validationSchema}
+                onSubmit={(values, { resetForm }) => {
+                    setTodos([...todos, values.task]);
+                    resetForm();
+                }}
+            >
+                {() => (
+                    <Form>
+                        <Field
+                            name="task"
+                            type="text"
+                            placeholder="Введіть задачу..."
+                        />
+                        <button type="submit">Додати</button>
+                        <div style={{ color: "red" }}>
+                            <ErrorMessage name="task" />
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+
+            {/* Список */}
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoApp;
